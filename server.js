c24286e44f124ff2bcab5ed80584c763a984d4b0
Brienyll <brienyll29@gmail.com>
2019-01-03 23:48:39 -0800

    got user profiles

--- server.js
+++ server.js
@@ -84,21 +84,20 @@ app.post('/register', (req, res) => {
   .then(user => {
   res.json(user[0]);
   })
-  .catch(err => res.status(400).json('unable to register'))
+  .catch(err => res.status(400).json('Unable to register'))
 })
 
 app.get('/profile/:id', (req, res) => {
   const { id } = req.params;
-  let found = false;
-  database.users.forEach(user => {
-    if (user.id === id) {
-      found = true;
-      return res.json(user);
+  db.select('*').from('users').where({id})
+  .then(user => {
+    if (user.length) {
+      res.json(user[0])
+    } else {
+      res.status(400).json('Not found')
     }
   })
-  if (!found) {
-    res.status(400).json('not found');
-  }
+  .catch(err => res.status(400).json('Error getting user'));
 })
 
 app.put('/image', (req, res) => {
