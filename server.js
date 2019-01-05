a24a3de92f5d79157fd9d652df77fa5b0b1cbca5
Brienyll <brienyll29@gmail.com>
2019-01-04 23:46:01 -0800

    connected postman and register

--- server.js
+++ server.js
@@ -73,18 +73,31 @@ bcrypt.compare("veggies", '$2a$10$K29lvv9vwk/gVEsOCk13pe8AaVTMsUXqogx5rzEhfEmoMw
 })
 
 app.post('/register', (req, res) => {
-  const { email, name } = req.body;
-  db('users')
-    .returning('*')
-    .insert({
-    email: email,
-    name: name,
-    joined: new Date()
-  })
-  .then(user => {
-  res.json(user[0]);
-  })
-  .catch(err => res.status(400).json('Unable to register'))
+  const { email, name, password } = req.body;
+  const hash = bcrypt.hashSync(password);
+    db.transaction(trx => {
+      trx.insert({
+        hash: hash,
+        email: email,
+      })
+      .into('login')
+      .returning('email')
+      .then(loginEmail => {
+        return trx('users')
+      .returning('*')
+      .insert({
+      email: loginEmail[0],
+      name: name,
+      joined: new Date()
+      })
+      .then(user => {
+      res.json(user[0]);
+        })
+      })
+      .then(trx.commit)
+      .catch(trx.rollback)
+    })
+    .catch(err => res.status(400).json('Unable to register'))
 })
 
 app.get('/profile/:id', (req, res) => {
