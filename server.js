bcf955c850156a5553772a34d2a27dc3eb1a6240
Brienyll <brienyll29@gmail.com>
2019-01-05 23:26:01 -0800

    finished signin post

--- server.js
+++ server.js
@@ -14,10 +14,6 @@ const db = knex({
   }
 });
 
-db.select('*').from('users').then(data => {
-  console.log(data);
-});
-
 const app = express();
 
 const database = {
@@ -56,20 +52,24 @@ app.get('/', (req, res) => {
 })
 
 app.post('/signin', (req, res) => {
-
-// Load hash from your password DB.
-bcrypt.compare("pony", '$2a$10$K29lvv9vwk/gVEsOCk13pe8AaVTMsUXqogx5rzEhfEmoMw.DGg47i', function(err, res) {
-  console.log('first guess', res)
-});
-bcrypt.compare("veggies", '$2a$10$K29lvv9vwk/gVEsOCk13pe8AaVTMsUXqogx5rzEhfEmoMw.DGg47i', function(err, res) {
-  console.log('second guess', res)
-});
-   if (req.body.email === database.users[0].email && 
-       req.body.password === database.users[0].password) {
-      res.json(database.users[0]);
-       } else {
-         res.status(400).json('error logging in');
-       }
+  db.select('email', 'hash').from('login')
+  .where('email', '=', req.body.email)
+    .then(data => {
+      const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
+      console.log(isValid);
+      if (isValid) {
+        return db.select('*').from('users')
+          .where('email', '=', req.body.email)
+          .then(user => {
+            console.log(user);
+            res.json(user[0])
+          })
+          .catch(err => res.status(400).json('unable to get user'))
+      } else {
+        res.status(400).json('wrong credentials')
+      }
+    })
+    .catch(err => res.status(400).json('wrong credentials'))
 })
 
 app.post('/register', (req, res) => {
