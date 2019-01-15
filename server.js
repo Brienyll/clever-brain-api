93f8d6ad57df1c5c8b3016449bb9bc23a0619365
Brienyll <brienyll29@gmail.com>
2019-01-15 00:08:32 -0800

    adding database info

--- server.js
+++ server.js
@@ -11,10 +11,8 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    host : 'postgresql-infinite-73730',
-    user : 'Brielle',
-    password : 'your_database_password',
-    database : 'smart-brain'
+    host : 'process.env.DATABASE_URL',
+    ssl: true,
   }
 });
 
